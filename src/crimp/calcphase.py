Important bug fix in local ephemerides calculation, improvement to other modules

src/crimp/calcphase.py
@@ -12,7 +12,7 @@ import os
 from math import factorial
 
 # Custom modules
-from crimp.readtimingmodel import ReadTimingModel
+from crimp.readtimingmodel import ReadTimingModel, get_parameter_value
 
 sys.dont_write_bytecode = True
 
@@ -25,7 +25,7 @@ class Phases:
         ----------
         timeMJD : float
             time array in modified julian day
-        timMod : str
+        timMod : str | dict
             timing model, i.e., .par file or a dictionary
 
         Methods
@@ -46,7 +46,7 @@ class Phases:
         ----------
             timeMJD : float
                 time array in modified julian day
-            timMod : str
+            timMod : str | dict
                 timing model, i.e., .par file or a dictionary
         """
         # original shape to restore later
@@ -65,16 +65,10 @@ class Phases:
     def _normalize_timdict(d):
         """
         Make sure the dict has the expected keys and numeric types
+        - Converts numeric values to float
+        - If a sub-dict contains both {'value', 'flag'}, replaces it with float(value), for ease of phase calculation
         """
-        out = {}
-        for k, v in d.items():
-            # try to coerce simple numeric values to float
-            if isinstance(v, (int, float, np.floating)):
-                out[k] = float(v)
-            else:
-                # keep sub-dicts (e.g., WAVE1: {'A':..., 'B':...}) as-is
-                out[k] = v
-        return out
+        return {k: get_parameter_value(v) for k, v in d.items()}
 
     def taylorexpansion(self):
         """

src/crimp/ephemIntegerRotation.py
@@ -1,8 +1,8 @@
 """
-A simple script that, given an MJD and a .par file, will provide
-the earliest MJD that results in an integer number of rotational
-phases from PEPOCH (epoch of timing solution) and corresponding
-ephemerides (currently only spin frequency)
+A simple module that, given an input MJD and a .par file, will provide
+the earliest MJD from the input MJD that results in an integer number
+of rotational phases from PEPOCH according to the input timing solution.
+It will also provide the ephemerides at that MJD, i.e., F0, and F1.
 
 Reminder that this script deals with taylor expansion of the phase
 evolution and a random number of glitches. Binary motion is not
@@ -14,8 +14,6 @@ Can be run from command line via "ephemintegerrotation"
 import argparse
 import sys
 
-import numpy as np
-
 # Custom modules
 from crimp.ephemTmjd import ephemTmjd
 from crimp.calcphase import calcphase
@@ -25,12 +23,12 @@ sys.dont_write_bytecode = True
 
 def ephemIntegerRotation(Tmjd, timMod, printOutput=False):
     """
-    Function that provides the earliest MJD and corresponding spin frequency
-    to input MJD, which, according to the input .par file, would result in an
-    integer number of rotational phases from PEPOCH
+    Function that provides the earliest MJD to input MJD, which,
+    according to the input .par file, would result in an integer
+    number of rotational phases from PEPOCH. It also provides the
+    corresponding F0 and F1 to the inferred MJD.
 
-    :param Tmjd: time for measurement of ephemerides that result
-                 in integer number of rotational phases.
+    :param Tmjd: time
     :type Tmjd: float
     :param timMod: .par file
     :type timMod: str
@@ -50,16 +48,16 @@ def ephemIntegerRotation(Tmjd, timMod, printOutput=False):
 
     Tmjd_intRotation = Tmjd - FracTFromIntRotation
     freq_Tmjd_intRotation = ephemTmjd(Tmjd_intRotation, timMod)["freqAtTmjd"]
+    freqdot_Tmjd_intRotation = ephemTmjd(Tmjd_intRotation, timMod)["freqdotAtTmjd"]
     ph_intRotation, _ = calcphase(Tmjd_intRotation, timMod)
-    ph_intRotation = np.round(ph_intRotation)
 
     if printOutput is True:
-        print('Input Tmjd = {} days. Corresponding spin frequency = {} Hz. Corresponding phase = {} \n'
-              'Earliest Tmjd with integer number of rotation = {}. Corresponding '
-              'frequency = {}. Corresponding phase = {}'.format(Tmjd, freq_Tmjd, ph_Tmjd, Tmjd_intRotation, freq_Tmjd_intRotation, ph_intRotation))
+        print(f"Input Tmjd = {Tmjd} days. Corresponding spin frequency = {freq_Tmjd} Hz. "
+              f"Corresponding phase = {ph_Tmjd} \n Earliest Tmjd with integer number of rotation = {Tmjd_intRotation}. "
+              f"Corresponding frequency = {freq_Tmjd_intRotation}. Corresponding phase = {ph_intRotation}")
 
     ephemerides_intRotation = {'Tmjd_intRotation': Tmjd_intRotation, 'freq_intRotation': freq_Tmjd_intRotation,
-                               'ph_intRotation': ph_intRotation}
+                               'freqdot_intRotation': freqdot_Tmjd_intRotation, 'ph_intRotation': ph_intRotation}
 
     return ephemerides_intRotation
 

src/crimp/ephemTmjd.py
@@ -1,5 +1,5 @@
 """
-A simple script to derive the ephemerides (only frequency at
+A simple script to derive the ephemerides (F and F_dot at
 the moment) at a given MJD based on a timing solution. Currently,
 this script takes into account taylor expansion of the phase
 evolution and a random number of glitches - binary motion is
@@ -32,15 +32,20 @@ def ephemTmjd(Tmjd, timMod):
     t0mjd = timModParam["PEPOCH"]
 
     #############################
-    # Taking into account the taylor expansion terms to the frequency evolution
+    # Taking into account the taylor expansion terms to F and F_dot evolution
     freqAtTmjd_te = timModParam["F0"]
     for nn in range(1, 13):
         freqAtTmjd_te += (1 / factorial(nn)) * timModParam["F" + str(nn)] * ((Tmjd - t0mjd) * 86400) ** nn
+
+    freqdotAtTmjd_te = timModParam["F1"]
+    for nn in range(2, 13):
+        freqdotAtTmjd_te += (1 / factorial(nn-1)) * timModParam["F" + str(nn)] * ((Tmjd - t0mjd) * 86400) ** (nn-1)
 
     ##################################
     # Taking into account the glitches
     nbrGlitches = len([gg for glKey, gg in timModParam.items() if glKey.startswith('GLEP_')])
     freqAtTmjd_gl = 0  # initializing the jump in frequency due to all glitches combined
+    freqdotAtTmjd_gl = 0  # initializing the jump in frequency due to all glitches combined
 
     for jj in range(1, nbrGlitches + 1):
         glep = timModParam["GLEP_" + str(jj)]
@@ -57,12 +62,16 @@ def ephemTmjd(Tmjd, timMod):
             # Here we calculate frequency shift according to each glitch model for all time column,
             # then we multiply by 0 if times are < glep and 1 if times are > glep using the boolean list created above
             freqAtTmjd_gl += (glf0 + (glf1 * ((Tmjd - glep) * 86400)) + (0.5 * glf2 * ((Tmjd - glep) * 86400) ** 2) +
-                              (glf0d * np.exp(-((Tmjd - glep) * 86400) / (gltd * 86400)))) * timesAfterGlitch
+                              (glf0d * np.exp(-(Tmjd - glep) / gltd))) * timesAfterGlitch
+            freqdotAtTmjd_gl += ((glf1 + (glf2 * ((Tmjd - glep) * 86400)) +
+                                 (-(glf0d/(gltd*86400)) * np.exp(-((Tmjd - glep) * 86400) / (gltd * 86400))))
+                                 * timesAfterGlitch)
 
     ######################################
     # Adding all frequency-shifts together
     freqAtTmjd = freqAtTmjd_te + freqAtTmjd_gl
+    freqdotAtTmjd = freqdotAtTmjd_te + freqdotAtTmjd_gl
 
-    ephemerides = {'Tmjd': Tmjd, 'freqAtTmjd': freqAtTmjd}
+    ephemerides = {'Tmjd': Tmjd, 'freqAtTmjd': freqAtTmjd, 'freqdotAtTmjd': freqdotAtTmjd}
 
     return ephemerides

src/crimp/fit_toas.py
@@ -27,19 +27,20 @@ import corner
 import argparse
 
 from crimp.calcphase import calcphase
-from crimp.readtimingmodel import ReadTimingModel, patch_par_values, patch_statistics, patch_miscellaneous
+from crimp.readtimingmodel import (ReadTimingModel, patch_par_values, patch_statistics, patch_miscellaneous,
+                                   get_parameter_value)
 from crimp.timfile import readtimfile, PulseToAs
 
 
 # -------- Starting point --------
-def load_toas_for_fit(tim_file: str, parfile: str, t_start: float | None = None, t_stop: float | None = None,
+def load_toas_for_fit(tim_file_df: pd.DataFrame, parfile: dict, t_start: float | None = None, t_stop: float | None = None,
                       t_mjd_phasewrap: float | None = None, mode: str = "add") -> pd.DataFrame:
     """
     Prepare the TOAs from a .tim file
-    :param tim_file: .tim TOA file in tempo2 format
-    :type tim_file: str
-    :param parfile: timing solution as a .par file
-    :type parfile: str
+    :param tim_file_df: .tim TOA dataframe (e.g., a .tim file read with timefile.py)
+    :type tim_file_df: pd.DataFrame
+    :param parfile: dictionary of timing solution (e.g., from ReadTimingModel)
+    :type parfile: dict
     :param t_start: load TOAs from t_start (in MJD)
     :type t_start: float | None
     :param t_stop: stop TOAs at t_stop (in MJD)
@@ -52,10 +53,13 @@ def load_toas_for_fit(tim_file: str, parfile: str, t_start: float | None = None,
     :rtype: pd.DataFrame
     """
     # Reading F0 to convert time uncertainties to phase uncertainties
-    F0 = readparfile(parfile)[0]['F0']
+    # "get_parameter_value" reads in the value whether the parfile dict has:
+    # - only value 'F0': 9.8765 (i.e., first return dictionary in ReadTimingModel)
+    # - or a dict of {'value': 9.8765, 'flag': 1} keys (i.e., third return dictionary in ReadTimingModel)
+    #  In both cases, F0 = 9.8765
+    F0 = get_parameter_value(parfile['F0'])
 
-    # Reading .tim file and store as an object of PulseToAs
-    tim_file_df = readtimfile(tim_file)
+    # Store .tim pulse TOA dataframe as an object of PulseToAs
     pt = PulseToAs(tim_file_df)
 
     # Filter for time if necessary
@@ -124,18 +128,6 @@ def add_phasewrap(toas_to_fit, t_mjd: float, mode: str = "add") -> pd.DataFrame:
     toas_to_fit["phase"] += sign * counts
 
     return toas_to_fit
-
-
-def readparfile(parfile: str):
-    """
-    Simple wrapper around ReadTimingModel which reads in a .par file as a list of dictionaries
-    :param parfile: timing solution as a .par file
-    :type parfile: str
-    :return: parameters as a dictionary without flags, and one that includes the flags
-    :rtype: tuple
-    """
-    parfile_params, _, parfile_params_flags = ReadTimingModel(parfile).readfulltimingmodel()
-    return parfile_params, parfile_params_flags
 
 
 # -------- Utilities --------
@@ -309,9 +301,8 @@ def validate_parfile(parfile):
 # -------- Model + Likelihood --------
 def model_phases(x_mjd, timmodel):
     """
-    Calculate the phases according to a timing model. This is done for fitting purposes:
-    We are fitting the phase shifts, and hence our timing model is some error on the timing parameters
-    which, when corrected for, the new timing solution will result in whitened residuals
+    Calculate mean-subtracted phases according to a timing model - for fitting purposes only
+    Use the module calcphase.py for all other purposes
     """
     phases, _ = calcphase(x_mjd, timmodel)  # full phases
     # Average so that after whitening, residuals align to 0,
@@ -661,13 +652,16 @@ def main():
 
     ###########################
     # reading TOAs and par file
-    toas_pre_fit = load_toas_for_fit(args.timfile_path, args.parfile, args.t_start, args.t_end, args.t_mjd, args.mode)
-    _, init_parfile = readparfile(args.parfile)
-    F0 = init_parfile['F0']['value']
+    init_parfile_withflags = ReadTimingModel(args.parfile).readfulltimingmodel()[2]
+    F0 = get_parameter_value(init_parfile_withflags['F0'])
+
+    tim_file_df = readtimfile(args.timfile_path)
+    toas_pre_fit = load_toas_for_fit(tim_file_df, init_parfile_withflags, args.t_start,
+                                     args.t_end, args.t_mjd, args.mode)
 
     ############################
     # Validate .par timing model
-    validate_parfile(init_parfile)
+    validate_parfile(init_parfile_withflags)
 
     START = toas_pre_fit["ToA"].min()
     FINISH = toas_pre_fit["ToA"].max()
@@ -676,7 +670,7 @@ def main():
     ########################################################################################
     # MCMC posteriors if desired (currently only way to get uncertainties on fit parameters)
     if args.mcmc:
-        keys = list_fit_keys(init_parfile)
+        keys = list_fit_keys(init_parfile_withflags)
         # check for the prior yaml file
         if args.mcmc and args.init_yaml is None:
             parser.error("-iy (or --init_yaml) is required when -mc (or --mcmc) is used")
@@ -685,7 +679,7 @@ def main():
         # Run MCMC with emcee
         print("Running MCMC with emcee...")
         sampler, flat, summaries = run_mcmc(x=toas_pre_fit["ToA"], y=toas_pre_fit["phase"],
-                                            yerr=toas_pre_fit["phase_err_cycle"], init_parfile=init_parfile,
+                                            yerr=toas_pre_fit["phase_err_cycle"], init_parfile=init_parfile_withflags,
                                             keys=keys, prior=prior, steps=args.mcmc_steps, burn=args.mcmc_burn,
                                             walkers=args.mcmc_walkers, corner_pdf=args.corner_pdf,
                                             chain_npy=args.chain_npy, flat_npy=args.flat_npy, progress=True)
@@ -699,7 +693,7 @@ def main():
 
         # prepare medians in keys order
         med_vec = np.array([summaries[name]['median'] for name in keys], dtype=float)
-        post_mcmc_timdict_fit, post_mcmc_timdict = inject_free_params(init_parfile, med_vec, keys)
+        post_mcmc_timdict_fit, post_mcmc_timdict = inject_free_params(init_parfile_withflags, med_vec, keys)
 
         source_label = "MCMC (posterior medians)"
 
@@ -737,7 +731,7 @@ def main():
         ##############################
         # Build the objective function
         nll, p0, keys, tmpl_parfile = make_nll(toas_pre_fit["ToA"], toas_pre_fit["phase"],
-                                               toas_pre_fit["phase_err_cycle"], init_parfile, args.init_yaml)
+                                               toas_pre_fit["phase_err_cycle"], init_parfile_withflags, args.init_yaml)
 
         ########################
         # MLE of above objective

src/crimp/get_local_ephem.py
@@ -12,9 +12,11 @@ import argparse
 
 from crimp.readtimingmodel import ReadTimingModel
 from crimp.ephemIntegerRotation import ephemIntegerRotation
+
 from crimp.fit_toas import (load_toas_for_fit, model_phases, inject_free_params, plot_residulas,
                             Prior, run_mcmc, list_fit_keys, chi2_fit)
 from crimp.plot_local_ephem import plot_local_ephemerides
+from crimp.timfile import readtimfile
 
 # Log config
 ############
@@ -58,7 +60,7 @@ def generate_local_ephemerides(
     :type outputfile: str | None
     :param ephem_plot: name of output .pdf plot of local F0, F1 vs time
     :type outputfile: str | None
-    :param clobber: override existing .txt file ('outputfile'.txt) of local F0, F1
+    :param clobber: override existing .txt file of local F0, F1? Default: False
     :type clobber: bool
     :return: local_ephem_final_df: dataframe of F0, F1, and MJD of each time interval with uncertainties
     :rtype: pd.DataFrame
@@ -86,14 +88,13 @@ def generate_local_ephemerides(
     glitch_epochs = sorted([value for key, value in parfile_params.items() if glitch_substring in key])
 
     # Reading TOAs from .tim file
-    toa_df = load_toas_for_fit(tim_file, parfile)
-    toas = toa_df['ToA']
+    toa_df = readtimfile(tim_file)
 
     # Start and stop times
     if t_start is None:
-        t_start = np.min(toas)
+        t_start = np.min(toa_df['pulse_ToA'])
     if t_end is None:
-        t_end = np.max(toas)
+        t_end = np.max(toa_df['pulse_ToA'])
 
     local_ephem_final = []
     current_start = t_start
@@ -105,17 +106,17 @@ def generate_local_ephemerides(
         current_end = min(current_start + interval_days, t_end)
 
         # Select ToAs between current_start and current_end
-        mask = (toa_df['ToA'] >= current_start) & (toa_df['ToA'] <= current_end)
-        df_interval = toa_df.loc[mask]
+        mask = (toa_df['pulse_ToA'] >= current_start) & (toa_df['pulse_ToA'] <= current_end)
+        toa_df_interval = toa_df.loc[mask]
 
-        if df_interval.empty:
+        if toa_df_interval.empty:
             # skip interval if no data but current_start < t_end is still true (e.g., long gap)
             current_start += jump_days
             continue
 
         # redefine current_start and current_end to match the exact days of TOA
-        current_start = df_interval['ToA'].min()
-        current_end = df_interval['ToA'].max()
+        current_start = toa_df_interval['pulse_ToA'].min()
+        current_end = toa_df_interval['pulse_ToA'].max()
 
         # Check if this interval crosses a glitch
         crossing_glitch = None
@@ -124,22 +125,29 @@ def generate_local_ephemerides(
                 crossing_glitch = g
                 break
 
+        # If we crossed a glitch indeed - refilter and redefine everything
         if crossing_glitch:
             # Truncate interval before glitch
             current_end = crossing_glitch
+            # re-filter data according to glitch_epoch
+            mask = toa_df_interval['pulse_ToA'] <= current_end
+            toa_df_interval = toa_df_interval.loc[mask]
+            # Redefine end more precisely
+            current_end = toa_df_interval['pulse_ToA'].max()
 
         # Exact time of mid-interval
         interval_mid = current_start + (current_end - current_start) / 2.0
         # Number of TOAs in this interval
-        n_toa_interval = len(df_interval)
+        n_toa_interval = len(toa_df_interval)
         # Length of time interval
         span_days = (current_end - current_start) if n_toa_interval > 0 else 0.0
         # if length of time interval > min_interval and number of TOA in interval is >= 4 then continue
         if (n_toa_interval >= 4) and (span_days > min_interval):
             # ---- Fit the corresponding subset of ToAs - from fit_toas.py ----
-            # Get F0 at mid-interval (note that I am trying to maintain the same phases for ease of fitting)
+            # Get F0 at mid-interval (simply to be as close as possible to correct solution for ease of fitting)
             ephem_tmp = ephemIntegerRotation(interval_mid, parfile)
             F0_mid = ephem_tmp['freq_intRotation']
+            F1_mid = ephem_tmp['freqdot_intRotation']
             interval_mid = ephem_tmp['Tmjd_intRotation']
 
             # Build a dictionary par file that fit_toas will understand
@@ -147,7 +155,7 @@ def generate_local_ephemerides(
             # Only caveat is that F0 to F12 must be provided - fill in F0, F1, and then padd 0 afterward
             all_keys = ["PEPOCH"] + [f"F{i}" for i in range(0, 13)]
             # Define the non-zero values for the first three keys
-            base_values = [interval_mid, F0_mid, F1_global]
+            base_values = [interval_mid, F0_mid, F1_mid]  # F0_mid
             base_flags = [0, 1, 1]
             # For the remaining keys (F2–F12), fill with zeros and flag=0
             n_extra = len(all_keys) - len(base_values)
@@ -163,32 +171,33 @@ def generate_local_ephemerides(
             fit_keys = list_fit_keys(init_par_file_dict)
             # Build the priors
             bounds = {
-                'F0': (-1 / (span_days * 86400), 1 / (span_days * 86400)),
-                'F1': (-1 / (span_days * 86400) ** 2, 1 / (span_days * 86400) ** 2),
+                'F0': (-10 / (span_days * 86400), 10 / (span_days * 86400)),
+                'F1': (-100 / (span_days * 86400) ** 2, 100 / (span_days * 86400) ** 2),
             }
             # Instantiate the Prior object - from fit_toas.py
             priors = Prior(bounds=bounds, initial_guess={})
 
             if debug_with_plots is True:
                 corner_plot = f"corner_interval_{int_counter}.pdf"
-                int_counter += 1
             else:
                 corner_plot = None
 
-            df_interval.loc[:, 'phase'] -= df_interval['phase'].mean()  # recenter things
-            _, _, summaries = run_mcmc(x=df_interval['ToA'], y=df_interval['phase'],
-                                       yerr=df_interval["phase_err_cycle"], init_parfile=init_par_file_dict,
+            # Create phases according to the above initial timing model
+            toas_to_fit = load_toas_for_fit(toa_df_interval, init_par_file_dict)
+            _, _, summaries = run_mcmc(x=toas_to_fit['ToA'], y=toas_to_fit['phase'],
+                                       yerr=toas_to_fit["phase_err_cycle"], init_parfile=init_par_file_dict,
                                        keys=fit_keys, prior=priors, steps=1000, burn=100, walkers=24,
                                        progress=False, corner_pdf=corner_plot)
 
             # Recover timing dict from best mcmc fit
             med_vec = np.array([summaries[name]['median'] for name in fit_keys], dtype=float)
             post_mcmc_timdict_fit, post_mcmc_timdict = inject_free_params(init_par_file_dict, med_vec, fit_keys)
-            phase_residulas_post_fit = model_phases(df_interval['ToA'], post_mcmc_timdict_fit)
+            phase_residulas_post_fit = model_phases(toas_to_fit['ToA'], post_mcmc_timdict_fit)
 
             if debug_with_plots is True:
                 # plot residuals after MCMC run
-                plot_residulas(df_interval, phase_residulas_post_fit, plotname=f"residuals_interval_{int_counter}")
+                plot_residulas(toas_to_fit, phase_residulas_post_fit, plotname=f"residuals_interval_{int_counter}")
+                int_counter += 1
 
             # Extract best-fit F0, F1 and their uncertainties
             # For error, convert to symmetric, take max of +/- q34 around q50
@@ -199,8 +208,8 @@ def generate_local_ephemerides(
 
             # Get statistics of best-fit for interval
             nbr_free_param = 2
-            simple_stats = chi2_fit(df_interval['phase'], phase_residulas_post_fit,
-                                    df_interval["phase_err_cycle"], nbr_free_param)
+            simple_stats = chi2_fit(toas_to_fit['phase'], phase_residulas_post_fit,
+                                    toas_to_fit["phase_err_cycle"], nbr_free_param)
 
             local_ephem_final.append({
                 'TOA_MJD_ref': interval_mid,

src/crimp/plot_local_ephem.py
@@ -5,10 +5,9 @@ Simple script to plot local ephemerides
 import pandas as pd
 import matplotlib.pyplot as plt
 import argparse
-import numpy as np
 
 
-def read_local_ephemerides(localephem: str, t_start=float | None, t_end=float | None):
+def read_local_ephemerides(localephem: str, t_start: float | None = None, t_end: float | None = None):
     """
     Read local ephemerides .txt file (build with get_local_ephem.py or CLT "localephemerides")
     :param localephem: .txt file of local ephemerides
@@ -24,11 +23,11 @@ def read_local_ephemerides(localephem: str, t_start=float | None, t_end=float | 
 
     # filter between t_start and t_end
     if t_start is None:
-        t_start = np.min(localephem_df_tmp["TOA_MJD_ref"])
+        t_start = localephem_df_tmp["TOA_MJD_ref"].min()
     if t_end is None:
-        t_end = np.max(localephem_df_tmp["TOA_MJD_ref"])
+        t_end = localephem_df_tmp["TOA_MJD_ref"].max()
 
-    mask = (localephem_df_tmp["TOA_MJD_ref"] >= t_start) & (localephem_df_tmp["TOA_MJD_ref"] <= t_end)
+    mask = ((localephem_df_tmp["TOA_MJD_ref"] >= t_start) & (localephem_df_tmp["TOA_MJD_ref"] <= t_end))
     localephem_df = localephem_df_tmp.loc[mask].reset_index(drop=True)
 
     return localephem_df

src/crimp/plot_pps.py
@@ -220,16 +220,27 @@ def plotting_phase_time(time_energy_phase_df, nphasebins=32, ntimebins=12,
     denom = row_max - row_min
     # Initialize output as NaN
     rate = np.full_like(H_T, np.nan, dtype=float)
-    # Compute normalized values where denom != 0 - time slice which does not have any data
+    # Compute normalized values where denom != 0, i.e., skip time slices which do not have any data
     np.divide(H_T - row_min, denom, out=rate, where=denom != 0)
 
-    # Optional Gaussian smoothing
+    # Optional Gaussian smoothing (a little more complicated than above since we may have time gaps)
     if smooth_sigma is not None:
-        # Apply smoothing in both phase (x) and energy (y) directions
-        # smooth_sigma can be a float or a list/tuple [sigma_energy, sigma_phase]
+        # float or list, which will be mapped to tuple (sigma_time, sigma_phase)
         if isinstance(smooth_sigma, list):
             smooth_sigma = tuple(smooth_sigma)
-        rate = gaussian_filter(rate, sigma=smooth_sigma, mode='nearest')
+
+        mask = np.isfinite(rate)
+        data = np.where(mask, rate, 0.0).astype(float)
+        wts = mask.astype(float)
+
+        # Convolve data and weights separately, then divide
+        data_blur = gaussian_filter(data, sigma=smooth_sigma, mode='nearest')
+        wts_blur = gaussian_filter(wts, sigma=smooth_sigma, mode='nearest')
+
+        with np.errstate(invalid='ignore', divide='ignore'):
+            rate_smooth = data_blur / wts_blur
+        # keep NaN where there was no support
+        rate = np.where(wts_blur > 0, rate_smooth, np.nan)
 
     # Plot
     fig, ax = plt.subplots(1, figsize=(12, 6), dpi=80, facecolor='w', edgecolor='k')